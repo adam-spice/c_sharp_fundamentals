87b80936526127511f44616c60da1012d16c4e8f
Adam Spice <adam@adam-spice/online>
2021-04-09 05:00:40 +1200

    just some of th basics

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -6,7 +7,40 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Adam!");
+            var x = 34.1;
+            var y = 17.2;
+            var result = x + y;
+            Console.WriteLine(result);
+
+            var numbers = new [] { 12.7, 10.3, 6.11 };
+            var numbersResult = 0.0;
+            foreach (double number in numbers)
+            {
+                numbersResult += number;
+            }
+            System.Console.WriteLine(numbersResult);
+
+            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+            grades.Add(56.1);
+
+            var gradesResult = 0.0;
+            foreach (double number in grades)
+            {
+                gradesResult += number;
+            }
+            gradesResult /= grades.Count;
+            System.Console.WriteLine($"The average grade is {gradesResult:N1}");
+
+
+
+            if(args.Length >0)
+            {
+                Console.WriteLine($"Hello, {args[0]}!");
+            }
+            else
+            {
+                Console.WriteLine("Hello!");
+            }
         }
     }
 }
